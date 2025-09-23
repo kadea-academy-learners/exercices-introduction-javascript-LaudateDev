38c658b48a1b5d86f281554ed61153c90ac3d268
LaudateDev <emmanuel.tasisu@gmail.com>
2025-09-23 11:30:56 +0200

    Résolution de 5 premiers exercices

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,6 @@
 // Exercice 5
 // Écrivez votre code ici
+let temperatureCelsius;
+let temperatureFahrenheit = (temperatureCelsius * 9) / 5 + 32;
+console.log(temperatureFahrenheit);
+temperatureCelsius = 100;
