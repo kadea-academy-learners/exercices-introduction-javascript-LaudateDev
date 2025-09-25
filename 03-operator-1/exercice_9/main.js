c6960eea359f9be0179951a6c9e161c3c2a32fbb
Beni Tasisu <emmanuel.tasisu@gmail.com>
2025-09-25 19:58:52 +0100

    Résolution de l'exercice 9 sur les parts d'héritage

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,2 +1,17 @@
 // Exercice 9
 // Écrivez votre code ici
+let valMaison = 60000000;
+let valTerrain = 40000000;
+let liquidites = 20000000;
+let totalHeritage = valMaison + valTerrain + liquidites;
+let partCategorie1 = totalHeritage * 0.75;
+let partCategorie2 = totalHeritage * 0.25;
+let paul = partCategorie1 / 3;
+let marie = paul;
+let eric = paul / 2;
+let clair = eric;
+let madameMukuna = partCategorie2 / 3;
+let joseph = madameMukuna;
+let sarah = madameMukuna;
+
+module.exports = { paul, marie, eric, clair, madameMukuna, joseph, sarah };
